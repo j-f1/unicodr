7e79c8c8dc306d5859ee4ebd44b88378e87573f1
Jed Fox <git@twopointzero.us>
2016-08-04 16:57:28 -0400

    ;

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-// jshint -W033
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 
 // const {"default": installExtension, REACT_DEVELOPER_TOOLS} = require('electron-devtools-installer');
@@ -12,20 +11,20 @@ const BrowserWindow = electron.BrowserWindow
 //   .then((name) => console.log(`Added Extension:  ${name}`))
 //   .catch((err) => console.log('An error occurred: ', err));
 
-const loadUnicodeData = require('./loader.js')
+const loadUnicodeData = require('./loader.js');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: 'hidden-inset', webPreferences: {
     scrollBounce: true
-  }})
+  }});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/ui/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/ui/index.html`);
 
   mainWindow.on('enter-full-screen', () => {
     mainWindow.webContents.executeJavaScript(`$('html').addClass('fullscreen')`);
@@ -35,30 +34,30 @@ function createWindow () {
 
   mainWindow.webContents.on('did-finish-load', () => {
     loadUnicodeData.then(data => {
-      mainWindow.webContents.send('UNICODE_DATA', data)
+      mainWindow.webContents.send('UNICODE_DATA', data);
     }, err => {
-      mainWindow.webContents.send('UNICODE_DATA.err', err)
-    })
-  })
+      mainWindow.webContents.send('UNICODE_DATA.err', err);
+    });
+  });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  app.quit()
-})
+  app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
