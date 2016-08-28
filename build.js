dd336c7e562e5241267f172bb430f8939ae299fa
Jed Fox <git@twopointzero.us>
2016-08-28 08:05:37 -0400

    build.js++

--- build.js
+++ build.js
@@ -2,7 +2,30 @@
 
 var packager = require('electron-packager');
 
-function prompt(promptStr, defaultValue) {
+/**
+ * USAGE:
+ * node build.js # will prompt for values
+ * node build.js --auto # defaults
+ * node build.js --plat <plat> # no prompt
+ * node build.js --arch <arch> # no prompt
+ * node build.js --plat # no prompt, default
+ * node build.js --arch # no prompt, default
+ */
+
+function prompt(arg, promptStr, defaultValue) {
+  if (process.argv.indexOf('--auto') > -1) {
+    return Promise.resolve(defaultValue);
+  }
+
+  let idx = process.argv.indexOf(arg);
+  if (idx > -1) {
+    let val = process.argv[idx+1];
+    if (!val || val.match(/^-+/)) {
+      val = defaultValue;
+    }
+    return Promise.resolve(val)
+  }
+
   return new Promise(function(resolve, reject) {
     process.stdout.write(`${promptStr}: [${defaultValue}] `);
     process.stdin.resume();
@@ -18,8 +41,8 @@ let fail = err => {
   console.error('ERR!', err);
 };
 
-prompt('Platform (linux/win32/darwin/all)', process.platform).then(platform => {
-  prompt('Architecture (ia32/x64/all)', process.arch).then(arch => {
+prompt('--plat', 'Platform (linux/win32/darwin/all)', process.platform).then(platform => {
+  prompt('--arch', 'Architecture (ia32/x64/all)', process.arch).then(arch => {
     packager({
       arch,
       dir: __dirname,
