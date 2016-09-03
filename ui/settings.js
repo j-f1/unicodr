b5f8bfb60ebe2f1e764cf5f6f893ce63c05820d5
Jed Fox <git@twopointzero.us>
2016-09-03 11:10:41 -0400

    Finish them Settings™.

---
+++ ui/settings.js
@@ -0,0 +1,25 @@
+function recursivelyApply(vals, f, _prefix) {
+  Object.keys(vals).forEach(name => {
+    let newName = (_prefix ? _prefix+'.':"") + name;
+    if (typeof vals[name] === "object") {
+      recursivelyApply(vals[name], f, newName);
+    } else {
+      f(newName, vals[name]);
+    }
+  });
+}
+settings.get().then(vals => {
+  recursivelyApply(vals, (key, value) => {
+    $(`[name="${key}"]`).removeAttr('disabled').prop('checked', value);
+  });
+});
+$('button').click(() => {
+  window.parent.$('.settings').removeClass('active');
+});
+$('.settings input').on('change', ({target: {name, checked}}) => {
+  try {
+    settings.set(name, checked);//.then(() => alert(`YAY! ${name}`), err => alert(`ERR! ${err}`));
+  } catch(e) {
+    alert(`FAIL ${e}`);
+  }
+});
