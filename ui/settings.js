d96391c1e19f01b0c8391a1a24cc255a6dda3c48
Jed Fox <git@twopointzero.us>
2016-12-10 18:37:24 -0500

    NEW: Wipe codepoint cache

--- ui/settings.js
+++ ui/settings.js
@@ -13,13 +13,32 @@ settings.get().then(vals => {
     $(`[name="${key}"]`).removeAttr('disabled').prop('checked', value);
   });
 });
-$('button').click(() => {
-  window.parent.$('.settings').removeClass('active');
+$('.settings .done').click(() => {
+  $('.settings').removeClass('active');
 });
 $('.settings input').on('change', ({target: {name, checked}}) => {
   try {
     settings.set(name, checked);
   } catch(e) {
     alert(`FAIL ${e}`);
+  }
+});
+$('.settings .cache').click(() => {
+  if (confirm('Really clear cache? Unicodr will quit.')) {
+    $('body').fadeOut(400, () => {
+      $('body').empty()
+               .css({margin: 0, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'})
+               .html('Clearing&hellip;')
+               .fadeIn(400, () => {
+        require('fs').unlink(require('../constants').CACHE_PATH, (err) => {
+          if (err) {
+            alert('Failed!\n'+err);
+          } else {
+            require('electron').remote.app.relaunch(); // tells Electron to restart after quitting.
+            require('electron').remote.app.exit(0);
+          }
+        });
+      });
+    });
   }
 });
